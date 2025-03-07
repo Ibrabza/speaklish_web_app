e20f7d12ad70064abf3ca00ae53a40a4b97911a8
Abzalbek Ibrokhimov <ibrohimov04@bk.ru>
2025-03-07 17:18:33 +0500

    fix lessons availability

--- src/components/LessonsList/LessonsList.tsx
+++ src/components/LessonsList/LessonsList.tsx
@@ -9,6 +9,7 @@ import Loading from "@/components/Loading.tsx";
 const LessonsList:FC = () => {
 
     const question_list = useSelector( (state:RootState) => state.user.question_list)
+    console.log("lessons",question_list?.lessons)
 
     if(!question_list) return <Loading/>
 
@@ -16,13 +17,13 @@ const LessonsList:FC = () => {
         <div className={styles.container}>
             <h3 className={styles.lesson_name}>{question_list?.course_title}</h3>
             <ul>
-                {question_list?.lessons?.map((item) => <li key={item.id}>
+                {question_list?.lessons?.map((item,index) => <li key={item.id}>
                     <LessonsListItem
                         index={item.id}
                         to={(item.title).split(" ").join("/")}
                         title={item.title}
                         date={item.start_date}
-                        status={item.is_completed}/>
+                        status={index === 0 ? true : question_list.lessons[index-1].is_completed}/>
                 </li>)}
             </ul>
         </div>
