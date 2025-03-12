a7d77ea76c90ad8ff17476878ccb35fe87d8b495
Abzalbek Ibrokhimov <ibrohimov04@bk.ru>
2025-03-12 16:36:11 +0500

    download file fix

--- src/components/Lesson/DownloadFile/DownloadFIle.tsx
+++ src/components/Lesson/DownloadFile/DownloadFIle.tsx
@@ -1,49 +1,22 @@
-import styles from "./DownloadFile.module.css";
-import { FC, useEffect } from "react";
+import styles from "./DownloadFile.module.css"
+import {FC} from "react";
 
 interface IDownloadFile {
-    fileName: string;
-    link: string;
+    fileName: string,
+    link: string,
 }
 
-const DownloadFile: FC<IDownloadFile> = (props) => {
-    useEffect(() => {
-        const downloadFile = async () => {
-            try {
-                const response = await fetch(props.link, {
-                    mode: "cors", // Ensure CORS is allowed
-                });
-                const blob = await response.blob();
-                const url = window.URL.createObjectURL(blob);
-
-                const link = document.createElement("a");
-                link.href = url;
-                link.setAttribute("download", props.fileName); // Force download
-                document.body.appendChild(link);
-                link.click();
-                document.body.removeChild(link);
-
-                // Revoke the object URL after download
-                window.URL.revokeObjectURL(url);
-            } catch (error) {
-                console.error("Download failed:", error);
-            }
-        };
-
-        if (props.link) {
-            downloadFile();
-        }
-    }, [props.link, props.fileName]);
-
+const DownloadFIle : FC<IDownloadFile> = (props) => {
     return (
-        <div className={styles.container}>
+        <a href={props.link} download className={styles.container}>
             <div className={styles.lesson_action__button}>
-                <img src={"/lesson2/folder.svg"} alt={"folder"} />
+                <img src={"/lesson2/folder.svg"} alt={"folder"}/>
                 <label htmlFor={"homework"}>{props.fileName}</label>
             </div>
-            <img src={"/lesson2/download.svg"} alt={"speaklish-icon-download"} />
-        </div>
-    );
-};
+            <img src={"/lesson2/download.svg"} alt={"speaklish-icon-download"}/>
+        </a>
+    )
+}
 
-export default DownloadFile;
+
+export default DownloadFIle;
