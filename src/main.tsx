e904b2822f10c4a8af369c5b9d4f27b40c0d1ba6
Abzalbek Ibrokhimov <ibrohimov04@bk.ru>
2025-03-22 18:24:32 +0500

    init function from sdk

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 // Import Telegram SDK initialization
-// import { init } from '@telegram-apps/sdk'
+import { init } from '@telegram-apps/sdk'
 
 // Initialize Telegram SDK
-// init();
+init();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
